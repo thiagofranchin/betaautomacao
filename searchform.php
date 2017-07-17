2b26bfcb30d95570f9272be00536e478854a2931
davi.callegari <callegari.davi@gmail.com>
2017-07-16 22:09:13 -0300

    btn busca

--- searchform.php
+++ searchform.php
@@ -10,10 +10,10 @@
 </form> -->
 
 <form class="form-inline my-2 my-lg-0" role="search" method="get" id="searchform" action="<?php bloginfo('siteurl'); ?>">
-  <div>
+  <div class="search-position">
     <label class="screen-reader-text" for="s"></label>
-    <input class="form-control mr-sm-2"	type="text" name="s" id="s" value="<?php echo get_search_query(); ?>" />
+    <input class="form-control mr-sm-2"	type="text" name="s" id="s" placeholder="Ex.: Cabo Termopar" value="<?php echo get_search_query(); ?>" />
     <?php // wp_dropdown_categories( 'show_option_all=All Categories' ); ?>
-    <button type="submit" id="searchsubmit" class="btn btn-outline-info my-2 my-sm-0">Buscar</button>
+    <button type="submit" id="searchsubmit" class="btn btn-primary my-2 my-sm-0"><i class="fa fa-search" aria-hidden="true"></i> Buscar</button>
   </div>
 </form>
