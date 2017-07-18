7b1e23142adb80161b55930cb8c07a2868db5ad5
Thiago Franchin <thiago.franchin@gmail.com>
2017-07-18 03:23:44 -0300

    page-contato.php page-a-beta-automacao.php

---
+++ page-a-beta-automacao.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+<div class="container content">
+<div class="row">
+				
+		<div class="col-8">
+			<h2>A Beta Automação 2</h2>
+			<h3><img class="size-full wp-image-283 aligncenter" src="http://res.cloudinary.com/beta-automacao/image/upload/v1457614328/banner2_03_vjhabv.jpg" alt="Beta Automação e Instrumentação" width="608" height="322" /></h3>
+<p>&nbsp;</p>
+<h3>A Beta Automação e Instrumentação</h3>
+<p>A Beta é uma empresa voltada ao fornecimento de soluções para o controle de temperatura em processos industriais. Seu fundador possui 15 anos de experiência em instrumentação e manutenção em diversos segmentos industriais. Atualmente, nosso foco é dedicar-se ao desenvolvimento de produtos voltados ao controle de temperatura através da potência – os <strong>relés de estado sólido.</strong></p>
+<p>Somos uma empresa nova, mas séria e comprometida em satisfazer as exigências de mercado, buscando novas tecnologias e aperfeiçoando as tecnologias atuais, conseguindo um produto de excelente qualidade e baixo custo. Nos colocamos à disposição para dúvidas pertinentes ao controle de temperatura em processos industriais</p>
+<h4><strong>Valores:</strong></h4>
+<p>Nossos valores são principalmente o respeito, a ética, o profissionalismo e dedicação por aquilo que fazemos</p>
+<h4><strong>Qualidade:</strong></h4>
+<p>Temos o compromisso com a qualidade de nossos produtos, que possuem certificação CE, além de passarem por testes antes de serem enviados aos nossos clientes</p>
+<h3>Parceiros</h3>
+<div class="accordion">
+<h3>Novus Produtos Eletronicos</h3>
+<div>
+<p>A Beta é  parceira da Novus. A Novus Produtos Eletrônicos é fabricante nacional de equipamentos para controle, aquisição, registro e supervisão de dados, com padrão internacional atuando principalmente no segmento de automação industrial.<br />
+<a href="http://res.cloudinary.com/beta-automacao/image/upload/v1457613434/parceiros1_cv2qfg.jpg"><img class="wp-image-640 size-full aligncenter" src="http://res.cloudinary.com/beta-automacao/image/upload/v1457613434/parceiros1_cv2qfg.jpg" alt="" width="600" height="150" /></a></p>
+</div>
+<h3>Huade América</h3>
+<div>
+<p>A  Huade América é especialista no fornecimento de produtos e serviços com principal atuação no mercado de <strong>hidráulica industrial</strong> e <strong>mobil</strong>. A Beta também especialista neste segmento fornece seus produtos exclusivos.</p>
+<p><img class="wp-image-641 size-full aligncenter" src="http://res.cloudinary.com/beta-automacao/image/upload/v1457613433/parceiros2_qhd6xq.jpg" alt="" width="600" height="150" /></p>
+</div>
+<h3>MD Micro Detectors</h3>
+<div>
+<p>A MIcro Detectors é especialista em todos os tipos de sensores, como: De Proximidade, Fotoelétricos, Ultrassônicos e Sensores de Área. E com a Beta você tem acesso a esta incrível tecnologia.</p>
+<p><img class="wp-image-725 size-full aligncenter" src="http://res.cloudinary.com/beta-automacao/image/upload/v1457613349/parceiros3_hizwvs.jpg" alt="Sensores Micro Detectors" width="600" height="150" /></p>
+</div>
+</div>
+		</div>
+
+			</div>
+</div>
+
+</div>
+<?php get_footer(); ?>
---
+++ page-contato.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div class="container content">
+    <div class="col-12">
+        <h2 class="pt-4">Fale com a Beta Automação, faça seu orçamento ou encomenda.</h2>
+
+        <p>Obrigado por nos escolher.</p>
+
+        <ul>
+            <li>+55 11 9.9512-8912 | 11 9.5859-5416</li>
+            <li>Segunda – Sexta das 8h às 18h</li>
+            <li>contato@betaautomacao. com.br| beta.representacoes@terra. com.br</li>
+            <li>Rua Estado do Rio de Janeiro, 326, Terras de S.Pedro e S.Paulo – CEP 13324-454 Salto/SP</li>
+        </ul>
+
+        <h2>Preencha o formulário e nos envie sua solicitação que em breve, entraremos em contato.</h2>
+    </div>
+    <div class="col-12 col-lg-4">
+        <div class="form-contato py-5">
+            <?php echo do_shortcode('[contact-form-7 id="116" title="Formulário Contato"]')?>
+        </div>
+    </div>
+
+</div>
+<?php get_footer(); ?>
